ad6a82015517ab0a5f1a7ee6cda57ab6afbf4040
sandrah <morandesandy@gmail.com>
2020-03-18 10:59:36 -0500

    superhero api ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-script
+
+
+function getSuperhero(heroSearch) {
+    var queryURL = "https://superheroapi.com/api/10218260924767452/search/" + heroSearch;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+
+  console.log(heroSearch)
+
+
+}
+getSuperhero("Batman")
