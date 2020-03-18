9f8ed3aeead4fe2dd1bde3478fd93bb26055f705
sandrah <morandesandy@gmail.com>
2020-03-18 13:59:05 -0500

    render image

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,43 @@
 
 
 function getSuperhero(heroSearch) {
-  var queryURL = "https://superheroapi.com/api.php/10218260924767452/search/" + heroSearch;
+    var queryURL = "https://superheroapi.com/api.php/10218260924767452/search/" + heroSearch;
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
     // console.log(response);
     getSuperheroImage(response)
+    displaySuperInfo(response);
+    searchNews(heroSearch);
+    
   });
+}
+// Function to dynamically display the superhero data on the page
+function displaySuperInfo(response){
+    // Empty the superhero info div
+    $("#super-info").empty();
 
+    // Grab the superhero data from the response
+    var heroData = response.results[0];
+
+    // Create elements for the data
+    var heroName = $("<h2>").text(heroData.name)
+    var fullName = $("<p>").text("Full Name: " + heroData.biography["full-name"]);
+    var connections = $("<p>").text("Connections: " + heroData.connections["group-affiliation"]);
+    var hQ = $("<p>").text("Headquartered in: " + heroData.work.base);
+    var firstAppeared = $("<p>").text("First appeared in: " + heroData.biography["first-appearance"]);
+    var publisher = $("<p>").text("Published by: " + heroData.biography.publisher);
+    // For the appearance, have sub-sections for gender, race, eye color, and hair color
+    var appearance = $("<div>");
+    var gender = $("<p>").text("Gender: " + heroData.appearance.gender);
+    var race = $("<p>").text("Race: " + heroData.appearance.race);
+    var eyeColor = $("<p>").text("Eye Color: " + heroData.appearance["eye-color"]);
+    var hairColor = $("<p>").text("Hair Color: " + heroData.appearance["hair-color"]);
+    // Add appearance data to appearance div.
+    appearance.append(gender, race, eyeColor, hairColor);
+    // Add the elements to the hero info div
+    $("#super-info").append(heroName, fullName, connections, hQ, firstAppeared, appearance, publisher);
 
 }
 getSuperhero("Batman");
@@ -31,8 +59,23 @@ function searchNews(heroSearch) {
     method: "GET"
   }).then(function (response) {
     // console.log(response);
+    renderNewsToPage(response);
   });
 
 }
 
-searchNews("Batman");
+function renderNewsToPage (response) {
+  for (let i = 0; i < response.articles.length; i++) {
+    // newsMain is for future use to hold the image and the main news info
+    var newsMain = $("<div>");
+    var newsInfo = $("<div>").addClass("news-info");
+    var newsHeading = $("<h5>").addClass("news-heading").text(response.articles[i].title);
+    var newsSourceDate = $("<h6>").text((response.articles[i].source.name) + " " + (new Date(response.articles[0].publishedAt).toLocaleDateString()));
+    var newsDescription = $("<p>").text(response.articles[i].description).addClass("news-description");
+  
+    newsMain.append(newsInfo.append(newsInfo, newsHeading, newsSourceDate, newsDescription));
+    $("#news-div").append(newsMain);
+    
+  }
+
+}
